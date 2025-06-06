f2eaffbc8b4aec6d2d4f801acb1f4c5789f1fef6
liht0 <liht0yager@gmail.com>
2025-06-06 23:20:43 +1000

    main.c

--- main.c
+++ main.c
@@ -1,10 +1,89 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main() {
+    int n;
+    printf("Введите размер вектора: ");
+    scanf("%d", &n);
+    
+    // Генерация вектора
+    int *vector = malloc(n * sizeof(int));
+    srand(time(NULL));
+    for(int i = 0; i < n; i++) {
+        vector[i] = rand() % 100;
+    }
 
-int main(int argc, char *argv[]) {
-	
-	printf("Hi students");
-	return 0;
+    // Запись в файл
+    FILE *file = fopen("input.txt", "w");
+    for(int i = 0; i < n; i++) {
+        fprintf(file, "%d\n", vector[i]);
+    }
+    fclose(file);
+    
+    // Преобразование чисел
+    FILE *in = fopen("input.txt", "r");
+    FILE *out = fopen("output.txt", "w");
+    int num;
+    while(fscanf(in, "%d", &num) != EOF) {
+        if(num % 5 == 0 && num % 7 == 0) fprintf(out, "ПЯТЬСЕМЬ\n");
+        else if(num % 5 == 0) fprintf(out, "ПЯТЬ\n");
+        else if(num % 7 == 0) fprintf(out, "СЕМЬ\n");
+        else fprintf(out, "%d\n", num);
+    }
+    fclose(in);
+    fclose(out);
+    
+    // Дальнейшая реализация (формирование матрицы, подсчет сумм)
+    // ...
+
+    return 0;
+}
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int n;
+    printf("Введите размер вектора: ");
+    scanf("%d", &n);
+    
+    // Генерация вектора
+    int *vector = malloc(n * sizeof(int));
+    srand(time(NULL));
+    for(int i = 0; i < n; i++) {
+        vector[i] = rand() % 100;
+    }
+
+    // Запись в файл
+    FILE *file = fopen("input.txt", "w");
+    for(int i = 0; i < n; i++) {
+        fprintf(file, "%d\n", vector[i]);
+    }
+    fclose(file);
+    
+    // Преобразование чисел
+    FILE *in = fopen("input.txt", "r");
+    FILE *out = fopen("output.txt", "w");
+    int num;
+    while(fscanf(in, "%d", &num) != EOF) {
+        if(num % 5 == 0 && num % 7 == 0) fprintf(out, "ПЯТЬСЕМЬ\n");
+        else if(num % 5 == 0) fprintf(out, "ПЯТЬ\n");
+        else if(num % 7 == 0) fprintf(out, "СЕМЬ\n");
+        else fprintf(out, "%d\n", num);
+    }
+    fclose(in);
+    fclose(out);
+    
+    // Дальнейшая реализация (формирование матрицы, подсчет сумм)
+    // ...
+
+    return 0;
 }
